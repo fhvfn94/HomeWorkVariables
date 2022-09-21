b45a8b8f5a3b124a3b9b4a4a9fb27072d5b615f5
fhvfn94 <fhvfn94@gmail.com>
2022-09-21 21:04:04 +0300

    3 task

---
+++ src/taskTwo.java
@@ -0,0 +1,13 @@
+public class taskTwo {
+    public static void tastwo(String[] args) {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
