f001be1e08dfbda4c8d0dde884acf050c26af09e
fhvfn94 <fhvfn94@gmail.com>
2022-09-22 22:16:47 +0300

    add sixTask

---
+++ src/sixTask.java
@@ -0,0 +1,8 @@
+public class sixTask {
+    public static void main(String[] args){
+        var one = 78.2;
+        var two = 82.7;
+        System.out.println(two - one);
+        System.out.println((one + two) % one);
+    }
+}
